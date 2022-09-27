622c5da13918cfd68ea7e54cbdcc033013fde6af
giovanni0077 <nefortun94@gmail.com>
2022-09-27 19:07:13 +0300

    Task Create Decimal Numbers with JavaScript

---
+++ task17.js
@@ -0,0 +1,11 @@
+// Task Create Decimal Numbers with JavaScript
+// ============================================
+// We can store decimal numbers in variables too. Decimal numbers are sometimes referred to as floating point numbers or floats.
+
+// Note: when you compute numbers, they are computed with finite precision. Operations using floating points may lead to different results than the desired outcome. If you are getting one of these results, open a topic on the freeCodeCamp forum.
+
+// Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+// ============================================
+
+// Solution by giovanni0077
+let myDecimal = 0.1;
