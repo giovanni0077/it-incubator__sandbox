13c113a9eddfdd0dced8732a82af4778ca150bbf
giovanni0077 <nefortun94@gmail.com>
2022-09-27 19:12:14 +0300

    Task Divide One Decimal by Another with JavaScript

---
+++ task19.js
@@ -0,0 +1,9 @@
+// Task Divide One Decimal by Another with JavaScript
+// ============================================
+// Now let's divide one decimal by another.
+
+// Change the 0.0 so that quotient will equal to 2.2.
+// ============================================
+
+// Solution by giovanni0077
+const quotient = 4.4 / 2.0;
