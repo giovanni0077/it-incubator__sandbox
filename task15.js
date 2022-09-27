cba635ed6f05a33f8a613e660f9dff1d535a2d74
giovanni0077 <nefortun94@gmail.com>
2022-09-27 18:58:24 +0300

    Task Increment a Number with JavaScript

---
+++ task15.js
@@ -0,0 +1,19 @@
+// Task Increment a Number with JavaScript
+// ============================================
+// You can easily increment or add one to a variable with the ++ operator.
+
+// i++;
+// is the equivalent of
+
+// i = i + 1;
+// Note: The entire line becomes i++;, eliminating the need for the equal sign.
+
+// Change the code to use the ++ operator on myVar.
+
+// let myVar = 87;
+// myVar = myVar + 1;
+// ============================================
+
+// Solution by giovanni0077
+let myVar = 87;
+myVar++;
