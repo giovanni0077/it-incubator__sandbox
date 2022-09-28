64b1d1adb5117bf005dff82cdc7901c14e490a6e
giovanni0077 <nefortun94@gmail.com>
2022-09-28 09:07:21 +0300

    Task Compound Assignment With Augmented Subtraction

---
+++ task22.js
@@ -0,0 +1,19 @@
+// Task Compound Assignment With Augmented Subtraction
+// ============================================
+// Like the += operator, -= subtracts a number from a variable.
+
+// myVar = myVar - 5;
+// will subtract 5 from myVar. This can be rewritten as:
+
+// myVar -= 5;
+// Convert the assignments for a, b, and c to use the -= operator.
+// ============================================
+
+// Solution by giovanni0077
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
