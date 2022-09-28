a5a53e480a8eaa40567a72c4b9f4a3d23d7d8c0d
giovanni0077 <nefortun94@gmail.com>
2022-09-28 13:11:54 +0300

    Task Nest one Array within Another Array

---
+++ task40.js
@@ -0,0 +1,12 @@
+// Task Nest one Array within Another Array
+// ============================================
+// You can also nest arrays within other arrays, like below:
+
+// const teams = [["Bulls", 23], ["White Sox", 45]];
+// This is also called a multi-dimensional array.
+
+// Create a nested array called myArray.
+// ============================================
+
+// Solution by giovanni0077
+const myArray = [["Ponny", 123],["Monkey", 321]];
