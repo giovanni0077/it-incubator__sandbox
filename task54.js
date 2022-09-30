1f625e9dd51fce0c79b0fe6e87b2ac36967965dd
giovanni0077 <nefortun94@gmail.com>
2022-09-30 09:21:08 +0300

    Task Global vs. Local Scope in Functions

---
+++ task54.js
@@ -0,0 +1,27 @@
+// Task Global vs. Local Scope in Functions
+// ============================================
+// It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
+
+// In this example:
+
+// const someVar = "Hat";
+
+// function myFun() {
+//   const someVar = "Head";
+//   return someVar;
+// }
+// The function myFun will return the string Head because the local version of the variable is present.
+
+// Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+// ============================================
+
+// Solution by giovanni0077
+const outerWear = "T-Shirt"
+
+function myOutfit() {
+  const myOutfit = "sweater"
+  const outerWear = myOutfit
+  return outerWear;
+}
+
+myOutfit()
