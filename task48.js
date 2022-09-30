ec1dd3333524eea16de581e7413779952861a0cf
giovanni0077 <nefortun94@gmail.com>
2022-09-30 08:22:28 +0300

    Task Shopping List

---
+++ task48.js
@@ -0,0 +1,18 @@
+// Task Shopping List
+// ============================================
+// Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+// The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+// ["Chocolate Bar", 15]
+// There should be at least 5 sub-arrays in the list.
+// ============================================
+
+// Solution by giovanni0077
+const myList = [
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15],
+  ["Chocolate Bar", 15]
+  ];
